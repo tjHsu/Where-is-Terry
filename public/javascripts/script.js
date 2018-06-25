88911685c43edef025a8cf674edd48bc1a0cc0df
Roberta Conrad <rjcnrd@gmail.com>
2018-06-25 19:12:41 +0200

    added display of all caught spots

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,227 +1,232 @@
-
-//define fake spots
+//define fake spots. should be passed through HBS file "in reality"
 let spot1 = {
-  name:"In front of MTV office",
+  name: "In front of MTV office",
   coordinates: {
-    latitude: 52.500120,
-    longitude: 13.453469,
+    latitude: 52.50012,
+    longitude: 13.453469
   }
-}
+};
 
 let spot2 = {
-  name:"Oberbaumbrücke",
+  name: "Oberbaumbrücke",
   coordinates: {
     latitude: 52.501982,
-    longitude: 13.445887,
+    longitude: 13.445887
   }
-}
+};
 
-
+let spots = [spot1, spot2];
 //create array of spots we want to display (later after database call)
 
 function startMap() {
-  const ironhackBER = {
-  	lat: 52.5003231,
-  	lng: 13.4514257};
-  const map = new google.maps.Map(
-    document.getElementById('map'),
-    {
-      zoom: 15,
-      center: ironhackBER,
-      styles: [
-        {
-            "featureType": "all",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "weight": "2.00"
-                }
-            ]
-        },
-        {
-            "featureType": "all",
-            "elementType": "geometry.stroke",
-            "stylers": [
-                {
-                    "color": "#9c9c9c"
-                }
-            ]
-        },
-        {
-            "featureType": "all",
-            "elementType": "labels.text",
-            "stylers": [
-                {
-                    "visibility": "on"
-                }
-            ]
-        },
-        {
-            "featureType": "landscape",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "color": "#f2f2f2"
-                }
-            ]
-        },
-        {
-            "featureType": "landscape",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                }
-            ]
-        },
-        {
-            "featureType": "landscape.man_made",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": -100
-                },
-                {
-                    "lightness": 45
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#eeeeee"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#7b7b7b"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "simplified"
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "labels.icon",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "transit",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "color": "#46bcec"
-                },
-                {
-                    "visibility": "on"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#c8d7d4"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#070707"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                }
-            ]
-        }
-    ],
-    }
-  );
+  const centerBerlin = {
+    lat: 52.5170874,
+    lng: 13.4019591
+  };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: centerBerlin,
+    styles: [
+      {
+        featureType: "all",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            weight: "2.00"
+          }
+        ]
+      },
+      {
+        featureType: "all",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#9c9c9c"
+          }
+        ]
+      },
+      {
+        featureType: "all",
+        elementType: "labels.text",
+        stylers: [
+          {
+            visibility: "on"
+          }
+        ]
+      },
+      {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#f2f2f2"
+          }
+        ]
+      },
+      {
+        featureType: "landscape",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      },
+      {
+        featureType: "landscape.man_made",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100
+          },
+          {
+            lightness: 45
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#eeeeee"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#7b7b7b"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#46bcec"
+          },
+          {
+            visibility: "on"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#c8d7d4"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#070707"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      }
+    ]
+  });
 
+  //Create a Marker for every spot in the spots array 
+  let spotMarkers = [];
+  let infoWindows = []; 
+
+  for (let i = 0; i < spots.length; i++) {
+    let newMarker = new google.maps.Marker({
+      position: {
+        lat: spots[i].coordinates.latitude,
+        lng: spots[i].coordinates.longitude
+      },
+      map: map,
+      title: spots[i].name
+    });
+    spotMarkers.push(newMarker);
+
+    // create infoWindow with description for each of the spots 
+
+    let infoWindow = new google.maps.InfoWindow({
+    content: spots[i].name
+    });
   
-  const spot1Marker = new google.maps.Marker({
-    position: {
-      lat: spot1.coordinates.latitude,
-      lng: spot1.coordinates.longitude,
-    },
-    map: map,
-    title: spot1.name
-  });
-  
-  const spot2Marker = new google.maps.Marker({
-    position: {
-      lat: spot2.coordinates.latitude,
-      lng: spot2.coordinates.longitude,
-    },
-    map: map,
-    title: spot2.name
-  });
+    infoWindows.push(infoWindow);
+
+    // add listener to each spot 
+
+    spotMarkers[i].addListener("click", function() {
+      infoWindows[i].open(map, spotMarkers[i]);
+    });
+  }
 }
-
-
-
 
 startMap();
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,31 @@ const express = require('express');
 const router  = express.Router();
 googleAPIKey = process.env.GOOGLE_API_KEY;
 
+//fake spots that will later be created based on user behavior
+
+let spot1 = {
+  name: "In front of MTV office",
+  coordinates: {
+    latitude: 52.50012,
+    longitude: 13.453469
+  },
+  description: "beautiful view of the spree river", 
+};
+
+let spot2 = {
+  name: "Oberbaumbrücke",
+  coordinates: {
+    latitude: 52.501982,
+    longitude: 13.445887
+  },
+  description: "live music", 
+};
+
+let spots = [spot1,spot2];
+
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index',{googleAPIKey});
+  res.render('index',{googleAPIKey : googleAPIKey, spots:spots });
 });
 
 module.exports = router;
