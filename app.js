9c84996a72fcba03879d7aafe9d1d4017a77ac34
tjHsu <tim800821@hotmail.com>
2018-06-26 17:56:44 +0200

    Bux Fixing in progress

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const flash      = require("connect-flash");
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/where-is-terry', {useMongoClient: true})
+  .connect(process.env.MONGODB_URI, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
