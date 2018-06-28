5d637860dd40e5f818eb812147e2fc3375bc92f9
tjHsu <tim800821@hotmail.com>
2018-06-28 12:47:13 +0200

    Safety checkpoint4

--- routes/add-place.js
+++ routes/add-place.js
@@ -1,7 +1,43 @@
 const express = require("express");
 const passport = require('passport');
-const authRoutes = express.Router();
-// const User = require("../models/User");
+const addRoutes = express.Router();
+const Spot = require("../models/Spot");
+
+
+
+addRoutes.get('/add', (req,res)=>{
+  res.render('auth/add-place');
+});
+
+addRoutes.post('/add', (req, res, next) => {
+
+  const {
+    name,
+    latitude,
+    longitude
+    
+  } = req.body;
+//   const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  // const imagePath =  `/uploads/${req.file.filename}`
+//   console.log(req.file);
+  const newSpot = new Spot({
+    name,
+    latitude,
+    longitude,
+    _creator:req.user._id
+  });
+
+
+  newSpot.save((err) => {
+      if (err){ next(null, false, { message: newSpot.errors }) }
+    //   return next(null, newPost);
+  })
+  .then(()=>{
+    res.redirect('/');
+  })
+
+  
+});
 
 
 
@@ -9,4 +45,6 @@ const authRoutes = express.Router();
 
 
 
-module.exports = authRoutes;
+
+
+module.exports = addRoutes;
